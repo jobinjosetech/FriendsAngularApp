e0b94b1c4e17cfd7d6b9da42fdf5b5c6970c2400
jobin <jobinjose.in@gmail.com>
2022-11-27 19:33:56 +0530

    navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AddFriendsComponent } from './add-friends/add-friends.component';
 import { ViewFriendsComponent } from './view-friends/view-friends.component';
 import { SearchFriendComponent } from './search-friend/search-friend.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddFriendsComponent,
     ViewFriendsComponent,
-    SearchFriendComponent
+    SearchFriendComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
